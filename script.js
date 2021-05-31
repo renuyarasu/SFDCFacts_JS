aa927b12b54a8801933ba159c86b0ff6653f371b
Renuka Prasad Yarasu <renukaprasad.yarasu@innroad.com>
2021-05-31 19:09:05 +0530

    JavaScript: Declaring and Defining Variables

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-console.log('The Ultimate Hands-On JavaScript');
-console.error('error');
-console.warn('warn');
+// JavaScript: Declaring and Defining Variables
+
+// var, let, const
+/**
+for (var i = 0; i < 10; i++) {
+    console.log(i); 
+}
+ */
+/**
+for (let i = 0; i < 10; i++) { 
+    console.log(i); 
+}
+ */
+const constValue = 10;
+constValue = 20;
+console.log(constValue); //10
+console.log(constValue); 
+//Uncaught TypeError: Assignment to constant variable [20].
+
