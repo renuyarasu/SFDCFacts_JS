4ede22e534a66837919eea46117c696928088f6f
Renuka Prasad Yarasu <renukaprasad.yarasu@innroad.com>
2021-06-06 05:47:36 +0530

    JavaScript: Data Types - String All methods

--- script.js
+++ script.js
@@ -1,12 +1,58 @@
-// JavaScript: Data Types
+// JavaScript: Data Types - String
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String
 
 // String • Number • BigInt • Boolean • undefined • null • Symbol • Object
 // https://www.programiz.com/javascript/data-types
 
-const student = "VedaGna";
-const studentRollNum = 3555;
-const studentIsPresent = true;
-const studentFee = null;
-const studentAge = undefined;
+let str = 'Lorem ipsum dolor sit amet consectetur adipisicing elit sit';
 
-console.log(typeof student);
+/* 
+// length
+let result = str.length;
+
+// toUpperCase
+let result = str.toUpperCase();
+
+// toLowerCase
+let result = str.toLowerCase();
+ 
+// includes - serach word in string - case-sensitive 
+let result = str.includes('sit') //true
+let result = str.includes('Sit') //flase
+
+// startsWith - must having 4 letters from string
+let result = str.startsWith('Lore') //true
+
+// endWith
+let result = str.endsWith('elit') // true
+
+// search
+let result = str.search('sit') //18
+
+// indexOf
+let result = str.indexOf('sit') //18
+
+// lastIndexOf
+let result = str.lastIndexOf('sit') //56
+
+// replace - replace working in string
+let result = str.replace('sit', 'stand')
+
+// concat
+let str2 = ' Hello World';
+let result = str + str2
+//Lorem ipsum dolor sit amet consectetur adipisicing elit sit Hello World
+
+// split - divide word with ','
+let result = str.split('renu'); //Lorem ipsum
+
+// slice
+let result = str.slice(0, 11) //Lorem ipsu
+
+// substr
+let result = str.substr(0, 20) //Lorem ipsum dolor si
+
+// substring
+let result = str.substring(0, 11); // Lorem ipsum
+*/
+console.log(result);
