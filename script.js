3b09ce13a9c190a477a3aa1054a23bc842d2235c
Renuka Prasad Yarasu <renukaprasad.yarasu@innroad.com>
2021-06-07 18:56:52 +0530

    Data Types - Objects JSON.stringify

--- script.js
+++ script.js
@@ -1,10 +1,33 @@
-// JavaScript: Data Types - Objects delete operator
+// JavaScript: Data Types - Objects JSON.stringify
+console.clear();
 
 const employee = {
-  name: "Employee",
+  name: "Employee01",
   id: 5,
   inOffice: true,
+  leaveDates: [5, 6, 7],
+  address: {
+    location: "HMT Hills",
+    street: "3rd street",
+    pincode: "500072",
+    citi: "Hyderabad",
+  },
 };
 
-delete employee.inOffice; // delete operator
-console.log(employee);
+const employees = [
+  employee,
+  {
+    name: "Employee02",
+    id: 15,
+    inOffice: false,
+    leaveDates: [30, 31],
+    address: {
+      location: "Jubilee Hills",
+      street: "1st street",
+      pincode: "500082",
+      citi: "Hyderabad",
+    },
+  },
+];
+
+console.log("Employees: " + JSON.stringify(employees));
