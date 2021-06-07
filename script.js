0e10b1bb7ac12dea9ab7c6c39de64f4056889247
Renuka Prasad Yarasu <renukaprasad.yarasu@innroad.com>
2021-06-07 13:53:04 +0530

    Data Types - Objects.entries/keys/values

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-// JavaScript: Data Types - Objects
+// JavaScript: Data Types - Objects.entries/keys/values
 
 const employee = {
   name: "employee_01",
   id: 5,
   inOffice: true,
 };
-console.log(employee);
+console.log(Object.entries(employee));
+console.log(Object.keys(employee));
+console.log(Object.values(employee));
