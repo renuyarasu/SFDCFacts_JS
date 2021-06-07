a5eb833c1416524ae484d07d41f5a61c17e5e6fc
Renuka Prasad Yarasu <renukaprasad.yarasu@innroad.com>
2021-06-07 22:18:00 +0530

    01. Function Declaration

--- script.js
+++ script.js
@@ -1,36 +1,13 @@
-// JavaScript: Data Types - Objects Template Literals
-console.clear();
+// JavaScript: Data Types - Functions
 
-const employee = {
-  name: "Anji Kota",
-  id: 5,
-  inOffice: true,
-  leaveDates: [5, 6, 7],
-  address: {
-    location: "HMT Hills",
-    street: "3rd street",
-    pincode: "500072",
-    citi: "Hyderabad",
-  },
-};
+/* 
+Three Ways to Define Functions in JavaScript
+  1. Function Declaration
+  2. Function Expression
+  3. Generator Function */
 
-const employees = [
-  employee,
-  {
-    name: "Renuka Prasad",
-    id: 15,
-    inOffice: false,
-    leaveDates: [30, 31],
-    address: {
-      location: "Jubilee Hills",
-      street: "1st street",
-      pincode: "500082",
-      citi: "Hyderabad",
-    },
-  },
-];
-
-console.log("Employees: " + JSON.stringify(employees));
-
-// String Concatenation and Template Literals
-console.log(`My Name is ${employee.name} and my ID ${employee.id}`);
+// 1. Function Declaration
+function greet() {
+  console.log("Bonjour Monsieur!");
+}
+greet();
