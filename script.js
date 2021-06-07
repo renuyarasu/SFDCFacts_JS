bdce1cc48b0feaae36583e73b2f12f3cb241a856
Renuka Prasad Yarasu <renukaprasad.yarasu@innroad.com>
2021-06-07 22:19:35 +0530

    2. Function Expression

--- script.js
+++ script.js
@@ -6,8 +6,16 @@ Three Ways to Define Functions in JavaScript
   2. Function Expression
   3. Generator Function */
 
-// 1. Function Declaration
+/* // 1. Function Declaration
 function greet() {
   console.log("Bonjour Monsieur!");
 }
+greet();
+ */
+
+// 2. Function Expression
+
+const greet = function () {
+  console.log("Bonjour Monsieur!");
+};
 greet();
