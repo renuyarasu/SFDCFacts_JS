5cddf80b4fab0045ba7de15ad3931956c512ca61
Renuka Prasad Yarasu <renukaprasad.yarasu@innroad.com>
2021-06-07 13:52:30 +0530

    JavaScript: Data Types - Objects

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-// JavaScript: Data Types - Array.sort method Using Arrow Function
+// JavaScript: Data Types - Objects
 
-const numbers = [1, 5, 8, 9, 4, 5, 6, 2, 3];
-
-numbers.sort((a, b )=> a - b);
-console.log(numbers);
+const employee = {
+  name: "employee_01",
+  id: 5,
+  inOffice: true,
+};
+console.log(employee);
