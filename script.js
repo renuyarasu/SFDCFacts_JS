ac4f875757cfeb14fe1a23780b1488616ae66db5
Renuka Prasad Yarasu <renukaprasad.yarasu@innroad.com>
2021-06-08 15:57:09 +0530

    Data Types - Comparison Operators

--- script.js
+++ script.js
@@ -1,18 +1,32 @@
-// JavaScript: Data Types - Compound Operators
+// JavaScript: Data Types - Comparison Operators
 // Arithmetic Operators || Assignment Operators || Comparison Operators || Logical Operators || Conditional Operators
-/* let x = 10;
-x = x + 10; // 20
-x += 10; // x = x + 10; 20
-x -= 10; // x = x - 10; 0
-x *= 10; // x = x * 10; 100
-x /= 10; // x = x / 10; 1
-console.log(x);
-  */
 
-const x = 'VedaGna ';
-const y = 100;
-// const z = x+y; //VedaGna 100
-const z = x-y; // NaN
-const z = x*y; // NaN
-const z = x/y; // NaN
-console.log(z);
+//Comparison Operators
+
+const x = 10;
+const y = '10';
+
+//equals operator
+console.log(x == y); // true
+
+//strict equals operator
+console.log(x === y); // false
+
+//not equals operator
+console.log(x != y); // false
+
+//strict not equals operator
+console.log(x !== y); // true
+
+//greater then operator
+console.log(x > y); // false
+
+//less then operator
+console.log(x < y); // false
+
+//greater then and equals to operator
+console.log(x >= y); // true
+
+//less then and equals to operator
+console.log(x <= y); // true
+
