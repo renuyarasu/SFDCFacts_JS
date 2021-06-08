d10acac895f0a6e6e95434837bf80133d71b976b
Renuka Prasad Yarasu <renukaprasad.yarasu@innroad.com>
2021-06-08 16:26:57 +0530

    Ternary Operator

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
-// JavaScript: Conditional Statements 
-    // If Else
+// JavaScript: Conditional Statements
+// Ternary Operator
 
-    const x = 19;
-    const y = 10;
+/*
+const x = 9;
+const y = 10;
 
-    if (x > y) {
-        console.log('X is bigger');
-    } else if(x<y){
-        console.log('Y is bigger');
-    } else{
-        console.log('Both are equal');
-    }
+if (x > y) {
+  console.log("X is bigger");
+} else if (x < y) {
+  console.log("Y is bigger");
+} else {
+  console.log("Both are equal");
+} */
+
+const x = 90;
+const y = 10;
+x > y
+  ? console.log("X is bigger")
+  : x < y
+  ? console.log("Y is bigger")
+  : console.log("Both are equal");
