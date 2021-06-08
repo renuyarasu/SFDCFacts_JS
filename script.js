1d8edcaf3580e34bff6bd2ed6e10c4e6a56dbc99
Renuka Prasad Yarasu <renukaprasad.yarasu@innroad.com>
2021-06-08 15:42:03 +0530

    Data Types - Compound Operators

--- script.js
+++ script.js
@@ -1,38 +1,9 @@
-// JavaScript: Data Types - Operator
-/*  Arithmetic Operators.
-    Assignment Operators.
-    Comparison Operators.
-    Logical Operators.
-    Conditional Operators.
- */
-console.clear();
-// Assignment Operators
-/* let x = 10;
-x = 20;
- */
-
-
-// Addition Operator
-/* let x = 10;
-let y = 20;
-let z = x+y;
-console.log(z);
- */
-
-// Subtraction Operator
-/* let x = 100;
-let y = 20;
-let z = x-y;
-console.log(z);
- */
-// Multiplication Operator
-/* let x = 10;
-let y = 20;
-let z = x*y;
-console.log(z); */
-
-// Division Operator
+// JavaScript: Data Types - Compound Operators
+// Arithmetic Operators || Assignment Operators || Comparison Operators || Logical Operators || Conditional Operators
 let x = 10;
-let y = 20;
-let z = x/y;
-console.log(z);
+// x = x + 10; // 20
+// x += 10; // x = x + 10; 20
+// x -= 10; // x = x - 10; 0
+// x *= 10; // x = x * 10; 100
+// x /= 10; // x = x / 10; 1
+console.log(x);
