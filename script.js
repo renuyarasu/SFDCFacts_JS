b50da4222d34f0711ae61c76870d06098fca275f
Renuka Prasad Yarasu <renukaprasad.yarasu@innroad.com>
2021-06-13 21:41:13 +0530

    Loops: For Loop - IN keyword using Objects

--- script.js
+++ script.js
@@ -1,41 +1,19 @@
 // JavaScript: Loops - For Loop
 
 // of keyword using Object
-const students = [
-  {
-    name: "Veda",
-    age: 25,
-    isPresent: true,
-  },
-  {
-    name: "Gnapika",
+const student = {
+    name: "VedaGna",
     age: 22,
-    isPresent: false,
-  },
-  {
-    name: "Hindu",
-    age: 26,
-    isPresent: true,
-  },
-  {
-    name: "Renu",
-    age: 35,
-    isPresent: false,
-  },
-  {
-    name: "Yarasu",
-    age: 25,
-    isPresent: true,
-  },
-  {
-    name: "Sai",
-    age: 19,
-    isPresent: false,
-  },
-];
-
-for (let student of students) {
-  // console.log(student); // Print all objects
-  // console.log(student.age); // Print only Ages
-  // console.log(student.name); // Print only Names
-}
+    fee: undefined,
+    dateAbsent: [25, 26, 27],
+    address: {
+      street: "HMT Hills",
+      pincode: 500072,
+      city: "Hyderabd",
+    },
+  };
+  
+  for (let prop in student) {
+    console.log(`$(prop): ${student[prop]}`);
+  }
+  
