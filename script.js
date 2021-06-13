ac0d4257c0eea89006168b47b33380f90ba6a111
Renuka Prasad Yarasu <renukaprasad.yarasu@innroad.com>
2021-06-13 22:43:45 +0530

    ES6 - Spread syntax (...) - Destructuring - Objects

--- script.js
+++ script.js
@@ -1,50 +1,20 @@
 // ES6 - Spread syntax (...)
 
-const students_01 = [
-  {
-    name: "Veda",
-    age: 25,
-    isPresent: true,
-  },
-  {
-    name: "Gnapika",
-    age: 22,
-    isPresent: false,
-  },
-  {
-    name: "Hindu",
-    age: 26,
-    isPresent: true,
-  },
-  {
-    name: "Renu",
-    age: 35,
-    isPresent: false,
-  },
-  {
-    name: "Yarasu",
-    age: 25,
-    isPresent: true,
-  },
-  {
-    name: "Sai",
-    age: 19,
-    isPresent: false,
-  },
-];
-const students_02 = [
-  {
-    name: "Murari",
-    age: 36,
-    isPresent: true,
-  },
-];
-const family = [...students_01, ...students_02];
-// console.log(family); // combining both arrays
+const person = {
+  name: "Veda",
+  age: 25,
+  isPresent: true,
+};
+
+const person_fee = {
+  fee: undefined,
+};
+
+const family = { ...person, ...person_fee };
+console.log(family); // combining key in both objects
 
 // Destructuring
-const [V, G, H] = family;
-console.log(V);
-console.log(G);
-console.log(H);
-// filter first 3 objects
+const {name, isPresent} = family;
+console.log(name);
+console.log(isPresent);
+// filter name & isPresent
