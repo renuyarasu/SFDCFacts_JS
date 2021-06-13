75f92c29259c8bcf61fcad918879e955e6983a0f
Renuka Prasad Yarasu <renukaprasad.yarasu@innroad.com>
2021-06-13 23:39:24 +0530

    JavaScript: OOPs - ES6 Classes

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
-// JavaScript: OOPs - Function Constructor
-function Person(personName, personGender, personDOB) {
-  this.name = personName;
-  this.gender = personGender;
-  this.dob = personDOB;
-  this.greet = function () {
+// JavaScript: OOPs - ES6 Classes
+
+class Person {
+  constructor(personName, personGender, personDOB) {
+    this.name = personName;
+    this.gender = personGender;
+    this.dob = personDOB;
+  }
+  greet() {
     console.log(`Hello ${this.name}`);
-  };
+  }
 }
 
 const person_01 = new Person("VedaGna", "Male", "24-02-2017");
 const person_02 = new Person("Gnapika", "Female", "26-12-2015");
+
 console.log(person_01);
 console.log(person_02);
+
 person_01.greet();
