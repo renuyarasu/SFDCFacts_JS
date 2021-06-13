96492b55596eb4310a4210eb98592c0d0c62ee2f
Renuka Prasad Yarasu <renukaprasad.yarasu@innroad.com>
2021-06-13 20:34:52 +0530

    Loops - continue statement

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 // For Loop
 const family = ["Chinnu", "Gnapika", "Veda", "Renu", "Yarasu"];
 for (let x = 0; x < family.length; x++) {
+  if (x === 2) continue;
   console.log(family[x]);
-  if (x === 3) break; //break statement
+  //   if (x === 3) break; //break statement
 }
