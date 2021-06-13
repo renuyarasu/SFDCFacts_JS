7c278071ff21c7b25740f183ffcabfcad702befa
Renuka Prasad Yarasu <renukaprasad.yarasu@innroad.com>
2021-06-13 20:47:41 +0530

    Loops: For Loop - OF keyword using Objects

--- script.js
+++ script.js
@@ -1,7 +1,41 @@
 // JavaScript: Loops - For Loop
 
-// of keyword using Arry
-const names = ["Chinnu", "Gnapika", "Veda", "Renu", "Yarasu"];
-for (let name of names) {
-  console.log(name);
+// of keyword using Object
+const students = [
+  {
+    name: "Veda",
+    age: 25,
+    isPresent: true,
+  },
+  {
+    name: "Gnapika",
+    age: 22,
+    isPresent: false,
+  },
+  {
+    name: "Hindu",
+    age: 26,
+    isPresent: true,
+  },
+  {
+    name: "Renu",
+    age: 35,
+    isPresent: false,
+  },
+  {
+    name: "Yarasu",
+    age: 25,
+    isPresent: true,
+  },
+  {
+    name: "Sai",
+    age: 19,
+    isPresent: false,
+  },
+];
+
+for (let student of students) {
+  // console.log(student); // Print all objects
+  // console.log(student.age); // Print only Ages
+  // console.log(student.name); // Print only Names
 }
