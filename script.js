924eb4c70838ebde648d882fa275e726eee4397d
Renuka Prasad Yarasu <renukaprasad.yarasu@innroad.com>
2021-06-14 13:37:49 +0530

    DOM - querySelector/ querySelectorAll

--- script.js
+++ script.js
@@ -3,5 +3,5 @@
 // getElementsBy
 // const querySelector = document.querySelector("#email"); // target Id from document
 // const querySelector = document.querySelector(".form-control"); // target first class element from document
-const querySelector = document.querySelectorAll(".form-control"); // target Id from document
+const querySelector = document.querySelectorAll(".form-control"); // target all class element from document
 console.log(querySelector);
