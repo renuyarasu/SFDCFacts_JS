751178f284e53e48265ba0acec03c6bed01639e4
Renuka Prasad Yarasu <renukaprasad.yarasu@innroad.com>
2021-06-14 14:02:47 +0530

    DOM - Working on input/button

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 // JavaScript: Working with DOM
 
 // getElementsBy
-// const querySelector = document.querySelector("#email"); // target Id from document
-// const querySelector = document.querySelector(".form-control"); // target first class element from document
-const querySelector = document.querySelectorAll(".form-control"); // target all class element from document
-console.log(querySelector);
+
+const button = document.querySelector("button");
+// button.remove(); // remove button
+// button.innerHTML = 'Delete';  // change button text
+(button.style.backgroundColor = "tomato"), (button.style.color = "white"); // change button styles
+console.log(button);
