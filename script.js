67decbd4d900cd5b9a2a793c2b45ad58c3ec3df4
Renuka Prasad Yarasu <renukaprasad.yarasu@innroad.com>
2021-06-14 08:34:53 +0530

    JavaScript: Working with DOM - Using Form

--- script.js
+++ script.js
@@ -1,20 +1,2 @@
-// JavaScript: OOPs - ES6 Classes
+// JavaScript: Working with DOM
 
-class Person {
-  constructor(personName, personGender, personDOB) {
-    this.name = personName;
-    this.gender = personGender;
-    this.dob = personDOB;
-  }
-  greet() {
-    console.log(`Hello ${this.name}`);
-  }
-}
-
-const person_01 = new Person("VedaGna", "Male", "24-02-2017");
-const person_02 = new Person("Gnapika", "Female", "26-12-2015");
-
-console.log(person_01);
-console.log(person_02);
-
-person_01.greet();
