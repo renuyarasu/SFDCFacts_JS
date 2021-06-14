b8a649ca771ebef866c37053ae5052287ae15685
Renuka Prasad Yarasu <renukaprasad.yarasu@innroad.com>
2021-06-14 14:07:24 +0530

    DOM - Add/ Remove class

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 // JavaScript: Working with DOM
 
-// getElementsBy
-
-const button = document.querySelector("button");
-// button.remove(); // remove button
-// button.innerHTML = 'Delete';  // change button text
-(button.style.backgroundColor = "tomato"), (button.style.color = "white"); // change button styles
-console.log(button);
+const container = document.querySelector(".container");
+container.classList.add("text"); // Add class
+container.classList.remove("container"); // Remove class
+console.log(container.classList);
