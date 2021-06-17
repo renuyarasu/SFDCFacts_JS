ff98a49285ba9b2e505c1b024a41bd5fd830c143
Renuka Prasad Yarasu <renukaprasad.yarasu@innroad.com>
2021-06-17 11:51:10 +0530

    MDN - Destructuring

--- script.js
+++ script.js
@@ -1,14 +1,7 @@
 // JavaScript: MDN - Destructuring
 
-let a, b, rest;
-[a, b] = [1, 2];
-console.log(a); //1
-console.log(b); //2
-[a, b, ...rest] = [1, 2, 3, 4, 5, 6];
-console.log(rest);
-({ a, b } = { a: 1, b: 2 });
+const x = [1, 2, 3, 4, 5];
+const [y, z] = x;
 
-console.log(a); //1
-console.log(b); //2
-({ a, b, ...rest } = { a: 1, b: 2, c: 3, d: 4, e: 5, f: 6 });
-console.log(rest);
+console.log(y); //1
+console.log(z); //2
