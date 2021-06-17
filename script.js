fc8c070928cdb63a97c9c03255dc36f47fd39636
Renuka Prasad Yarasu <renukaprasad.yarasu@innroad.com>
2021-06-17 11:48:12 +0530

    MDN - Destructuring

--- script.js
+++ script.js
@@ -6,3 +6,9 @@ console.log(a); //1
 console.log(b); //2
 [a, b, ...rest] = [1, 2, 3, 4, 5, 6];
 console.log(rest);
+({ a, b } = { a: 1, b: 2 });
+
+console.log(a); //1
+console.log(b); //2
+({ a, b, ...rest } = { a: 1, b: 2, c: 3, d: 4, e: 5, f: 6 });
+console.log(rest);
