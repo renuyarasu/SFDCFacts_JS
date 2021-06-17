1a8a7b18bb8b9e24d9489aff6244810eec35e9a3
Renuka Prasad Yarasu <renukaprasad.yarasu@innroad.com>
2021-06-17 12:40:19 +0530

    MDN - Destructuring - Combined Array and Object Destructuring - FizzBuzz

--- script.js
+++ script.js
@@ -1,28 +1,13 @@
 // JavaScript: MDN - Destructuring
-// For of iteration and destructuring
-const people = [
-  {
-    name: "VedaGna",
-    family: {
-      father: "Renu",
-      mother: "Hindu",
-      sister: "Gnapika",
-    },
-    age: 4
-  },
-  {
-    name: "Renu",
-    family: {
-      father: "Subbarao",
-      mother: "Venkateswaramma",
-      sister: "Tejaswi",
-    },
-    age: 35
-  },
+// Combined Array and Object Destructuring
+//FizzBuzz
+console.clear();
+const props = [
+  { id: 1, name: "Fizz" },
+  { id: 2, name: "Buzz" },
+  { id: 3, name: "FizzBuzz" },
 ];
-
-for( const {name:n, family: {father: f}} of people){
-  console.log('Name: ' + n + ', Father: '+ f);
-}
-// Name: VedaGna, Father: Renu
-// Name: Renu, Father: Subbarao
+const [, , {name}] = props;
+console.log(name);
+const [, , {id}] = props;
+console.log(id);
