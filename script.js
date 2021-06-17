8cea9790e3adf1893358a329a5569fa36eb49e9d
Renuka Prasad Yarasu <renukaprasad.yarasu@innroad.com>
2021-06-17 12:07:06 +0530

    MDN - Destructuring - Object destructuring

--- script.js
+++ script.js
@@ -1,21 +1,14 @@
 // JavaScript: MDN - Array: Destructuring
-
-const foo = [1, 2, 3];
-
-const [red, green, yellow] = foo;
-console.log(red); // 1
-console.log(green); // 2
-console.log(yellow); // 3
-
-// Swapping Variables
-let a = 5;
-let b = 10;
-[a, b] = [b, a];
-console.log(a);
-console.log(b);
-
-const myArr = [1,2,3];
-[myArr[2], myArr[1]] = [myArr[1], myArr[2]];
-console.log(myArr); //[ 1, 3, 2 ]
+console.clear();
+// Object destructuring
+const user = {
+  id: 42,
+  is_verified: true,
+};
+const { id, is_verified } = user;
+console.log(is_verified);
 
 
+const {a = 10, b=20} = {a:100}
+console.log(a); // 3
+console.log(b); // 5
