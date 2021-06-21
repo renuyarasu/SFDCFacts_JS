88495688ba12f39b31f7957d63e633a10cb7d8d7
Renuka Prasad Yarasu <renukaprasad.yarasu@innroad.com>
2021-06-21 15:25:49 +0530

    JavaScript: Destructuring

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
-// JavaScript: Object Literals
+// JavaScript: Destructuring and RestSpread
+console.clear();
 
-const mystery = 'answer';
-const inversePI = 1 / Math.PI;
+// const PI = Math.PI;
+// const E = Math.E;
+// const SQRT2 = Math.SQRT2;
 
-const obj = {
-    p1: 10,
-    p2: 20,
-    f1() { },
-    f2: () => { },
-    [mystery]: 42,
-    inversePI,
-}
+const { PI, E, SQRT2 } = Math;
 
-console.log(obj.inversePI); // 42
+const circle = {
+    label: 'circleX',
+    radius: 2,
+};
+const circleArea = ({ radius }, { precision = 2 } = {}) =>
+    (PI * radius * radius).toFixed(precision);
+
+console.log(circleArea(circle)); // 12.57
+console.log(circleArea(circle, { precision: 5 })); // 12.56637
