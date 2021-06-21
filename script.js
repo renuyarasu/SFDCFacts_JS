68eb68b7b11c61aae58a4670f4a88edf3421bfad
Renuka Prasad Yarasu <renukaprasad.yarasu@innroad.com>
2021-06-21 15:11:57 +0530

    JavaScript: Object Literals - inversePI

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 // JavaScript: Object Literals
+
 const mystery = 'answer';
+const inversePI = 1 / Math.PI;
+
 const obj = {
     p1: 10,
     p2: 20,
     f1() { },
     f2: () => { },
     [mystery]: 42,
+    inversePI,
 }
 
-console.log(obj.answer); // undefined
+console.log(obj.inversePI); // 42
